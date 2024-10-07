d85b0551b5ebda4badd1a58eab0ffef4a1f57e36
Amelie Girard <agirard@lightning1.ihpc.uts.edu.au>
2024-10-08 09:44:26 +1100

    check

--- check.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load your CSV file
-csv_path = "/data/agirard/Projects/Timetravel/results/zero_shot_results.csv"
-data = pd.read_csv(csv_path)
-
-# Print the column names
-print("Columns in CSV:", data.columns)
